3dbe19556b05c53bf8863348c9ba1fc77310a1ae
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-26 05:37:40 +0530

    Azure SDK

--- go-azure-app/main.go
+++ go-azure-app/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/compute/mgmt/compute"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
-}
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Azure World %s", r.URL.Path[1:])
+	// Create an Azure authorizer from environment variables or Azure Managed Service Identity
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		fmt.Println("Failed to create authorizer:", err)
+		return
+	}
+
+	// Create a compute client using the authorizer
+	vmClient := compute.NewVirtualMachinesClient("Actual Subscription ID")
+	vmClient.Authorizer = authorizer
+
+	// List the virtual machines in the subscription
+	vms, err := vmClient.List(context.Background(), "Actual Resource Group Name")
+	if err != nil {
+		fmt.Println("Failed to list virtual machines:", err)
+		return
+	}
+
+	// Print the virtual machine names
+	for _, vm := range vms.Values() {
+		fmt.Println(*vm.Name)
+	}
 }
